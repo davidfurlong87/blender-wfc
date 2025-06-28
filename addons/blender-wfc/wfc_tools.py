fbfc3141bcb218fee9d2e3d263f518d524746813
Heldrun <dfurlongd@gmail.com>
2025-06-28 13:57:08 +0100

    proper primitives and cleanup

--- addons/blender-wfc/wfc_tools.py
+++ addons/blender-wfc/wfc_tools.py
@@ -22,10 +22,72 @@ from bpy.props import (
 #        StringProperty,     
 )
 
+
+def clear_collection(collection):
+    if collection:
+        for obj in collection.objects:
+            bpy.data.objects.remove(obj, do_unlink=True)
+
+
+def initialize_collection(collection_name):
+    if collection_name in bpy.data.collections:
+        collection = bpy.data.collections[collection_name]
+        clear_collection(collection)
+    else:
+        collection = bpy.data.collections.new(collection_name)
+        scene = bpy.context.scene
+        scene.collection.children.link(bpy.data.collections[collection_name])
+    return collection
 # ----------------------------------------------
 # Define Basic Python Classes
 # ----------------------------------------------
+x_size = 5
+y_size = 5
+plane_size = 1
 
+plane_dict = {}
+
+collection = initialize_collection("collection_name")
+
+def get_cell(x, y):
+    return plane_dict.get(x, {}).get(y)
+
+
+def delete_cell(x, y):
+    specific_plane = get_cell(x, y)
+    if specific_plane:
+        collection.objects.unlink(specific_plane)
+        del plane_dict[x][y]
+        bpy.data.objects.remove(specific_plane, do_unlink=True)
+
+
+def main(context):
+    collection = initialize_collection()
+
+    for x in range(x_size):
+        plane_dict[x] = {}
+        for y in range(y_size):
+            location = (x * (plane_size * 2), y * (plane_size * 2), 0)
+
+            bpy.ops.object.empty_add(location=location)
+            empty = bpy.context.object
+            empty.name = f"{x:02d}_{y:02d}_holder"
+            bpy.context.collection.objects.unlink(empty)
+            collection.objects.link(empty)
+
+            bpy.ops.mesh.primitive_plane_add(scale=(plane_size, plane_size, 1), enter_editmode=False, align='WORLD',
+                                             location=location)
+            plane = bpy.context.object
+            plane.name = f"{x:02d}_{y:02d}_plane"
+            bpy.context.collection.objects.unlink(plane)
+            collection.objects.link(plane)
+            plane.parent = empty
+            plane.location = (0, 0, 0)
+
+            plane_dict[x][y] = plane
+
+    print(get_cell(x, y))
+    delete_cell(4, 3)
 
 class Cell:
     def __init__(self, posX, posY, possibleVariantIds, possibleVariants):
@@ -91,20 +153,6 @@ prefab_variant_collection_name = "PrefabVariants"
 # ----------------------------------------------
 # General/Universal Functions
 # ----------------------------------------------
-
-def clear_collection(collection):
-    for obj in collection.objects:
-        bpy.data.objects.remove(obj, do_unlink=True)
-        
-def initialize_collection(collection_name):
-    scene = bpy.context.scene
-    if collection_name in bpy.data.collections:
-        collection = bpy.data.collections[collection_name]
-        clear_collection(collection)
-    else:
-        collection = bpy.data.collections.new(collection_name)
-        scene.collection.children.link(collection)
-    return collection
 
 def instantiate_random_object(xPos, yPos, prefabSize, grid_objects_collection, variant_collection, cell):
 #    chosenVariant = cell.possibleVariants[randint(0, len(cell.possibleVariants)-1)]
